1b00c2ccdaa6dadd106dafe658dd7db5b1afb784
HameemH <hameemhossain2@gmail.com>
2025-07-25 13:47:46 +0600

    project created & db connected

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/db', function () {
+    try {
+        DB::connection()->getPdo();
+        return " Database connected successfully.";
+    } catch (\Exception $e) {
+        return " Could not connect to the database. Error: " . $e->getMessage();
+    }
+});
+Route::get('/', function () {
+    return view('welcome');
+});
